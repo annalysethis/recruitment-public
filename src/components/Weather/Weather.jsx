e80794b282f8308056f1575fb8c8ee25e124b1d0
annalysethis <annalys01@gmail.com>
2022-01-25 11:52:51 +0100

    finished assignment

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,41 +1,79 @@
-import React, { useState } from 'react';
-import classes from './Weather.css';
+import React, { useState } from "react";
+import "./Weather.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 
 const Weather = () => {
+  let [location, setLocation] = useState();
+  let [currentWeather, setCurrentWeather] = useState({});
 
-    let [location, setLocation] = useState();
-    let [currentWeather, setCurrentWeather] = useState({});
-    
-    function getCurrentWeather(e) {
-        e.preventDefault();
+  function getCurrentWeather(e) {
+    e.preventDefault();
 
-        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=a17480f70f0d4368ad0b5eabd0e37b66`, {
-            "method": "GET"            
-        })
-        .then(response => response.json())
-        .then(response => {
-            setCurrentWeather(response);    
-        });
-    }
-
-    return (
-        <div>
-            <h2>How's the weather out there?</h2>
-            <form onSubmit={getCurrentWeather}>
-                <input
-                    type="text"
-                    placeholder="Enter City"
-                    maxLength="50"
-                    className={classes.textInput}
-                    value={location}
-                    onChange={(e) => setLocation(e.target.value)}
-                    />                
-               
-                <button className={classes.Button} type="submit">Get Weather</button>
-            </form>
-            <div><pre>{JSON.stringify(currentWeather, null, 2) }</pre></div>            
-        </div>
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=a17480f70f0d4368ad0b5eabd0e37b66`,
+      {
+        method: "GET",
+      }
     )
-}
+      .then((response) => response.json())
+      .then((response) => {
+        setCurrentWeather(response);
+      });
+  }
+
+  const calCelsius = () => {
+    try {
+      const kelvin = currentWeather.main.temp;
+      const celsius = Math.round(kelvin - 273.15);
+      if (kelvin === null || celsius === null) {
+        return false;
+      }
+      return <h1>{celsius}&deg;</h1>;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
+
+  return (
+    <div>
+      <h2>How's the weather out there?</h2>
+      <form onSubmit={getCurrentWeather}>
+        <input
+          type="text"
+          placeholder="Enter City"
+          maxLength="50"
+          className="textInput"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+
+        <button class="Button" type="submit">
+          Get Weather
+        </button>
+      </form>
+      <div>
+        <div className="Wrapper">
+          <h1>{currentWeather.name ? `${currentWeather.name}` : ""}</h1>
+          <FontAwesomeIcon className="Marker" icon={faMapMarkerAlt} />
+        </div>
+        <div className="Wrapper">
+          {calCelsius()}
+          {calCelsius() ? (
+            <img
+              className="Icon"
+              src={`http://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png`}
+              alt=""
+            />
+          ) : (
+            ""
+          )}
+        </div>
+        <h3>{!currentWeather.name ? "please enter a location..." : ""}</h3>
+      </div>
+    </div>
+  );
+};
 
 export default Weather;
